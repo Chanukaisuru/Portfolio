fb0e1c60ce0520cf8c6ff19540feef745ca8e1fa
Chanukaisuru <chanukaisuru26@gmail.com>
2024-10-20 20:01:33 +0530

    modified:   portfolio/src/App.css 	modified:   portfolio/src/App.js 	modified:   portfolio/src/components/about/about.css 	new file:   portfolio/src/components/skills/Skills.jsx 	new file:   portfolio/src/components/skills/skills.css

---
+++ portfolio/src/components/skills/Skills.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section className='skills section' id='skills'>
+        <h2 className="section_title"> Skills</h2>
+        <span className="section_subtitle">My Technical level</span>
+
+        <div className='skills_container container grid'>
+            
+        </div>
+
+    </section>
+  );
+};
+
+export default Skills;
