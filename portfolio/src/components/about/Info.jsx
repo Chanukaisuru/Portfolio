9efc8eddde1a6d7ad4fef08c65596fc22f1b7ef9
Chanukaisuru <chanukaisuru26@gmail.com>
2024-10-20 19:26:13 +0530

    modified:   portfolio/src/App.css 	modified:   portfolio/src/assets/about.jpg 	deleted:    portfolio/src/assets/my3.jpg 	modified:   portfolio/src/components/about/Info.jsx 	modified:   portfolio/src/components/about/about.css

--- portfolio/src/components/about/Info.jsx
+++ portfolio/src/components/about/Info.jsx
@@ -5,16 +5,21 @@ const Info = () => {
     <div className='about_info grid'>
       <div className="about_box">
         <i class='bx bx-award'></i>
+
         <h3 className="about_title">Experience</h3>
-        <span className="about_subtitle">* year Working</span>
+        <span className="about_subtitle">Year Working</span>
       </div>
 
       <div className="about_box">
+        <i class='bx bx-briefcase-alt'></i>
+
         <h3 className="about_title">Completed</h3>
         <span className="about_subtitle">3 + Projects</span>
       </div>
 
       <div className="about_box">
+        <i class="bx bx-support"></i>
+        
         <h3 className="about_title">Support</h3>
         <span className="about_subtitle">Online 24/7</span>
       </div>
