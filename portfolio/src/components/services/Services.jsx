dab4586ed95426fa33cf0e1cbe39456161093a24
Chanukaisuru <chanukaisuru26@gmail.com>
2024-10-27 16:11:45 +0530

    modified:   portfolio/src/components/home/home.css 	modified:   portfolio/src/components/services/Services.jsx 	modified:   portfolio/src/components/services/services.css

--- portfolio/src/components/services/Services.jsx
+++ portfolio/src/components/services/Services.jsx
@@ -13,69 +13,6 @@ const Services = () => {
         <span className="section_subtitle">What I Offer</span>
 
         <div className="services_container sontainer grid">
-            <div className="services_content">
-                <div>
-                    <i className="uil uil-web-grid services_icon"></i>
-                    <h3 className="services_title">
-                        product <br/>Designer</h3>
-                </div>
-
-                <span className="services_button" onClick={() =>
-                    toggleTab(1)}>
-                    View More
-                    <i className="uil uil-arrow-right services_button-icon"></i></span>
-
-                <div className={toggleState===1? "services_modal active-modal" : "services_modal"}>
-                    <div className="services_modal-content">
-                        <i onClick={() => toggleTab(0)}className="uil uil-times services_modal-close"></i>
-
-                        <h3 className="services_modal-title">product Designer</h3>
-                        <p className="services_modal-description">
-                            service with more than 3 years of experience.
-                            providing quality work to clients and companies.
-                        </p>
-
-                        <ul className="services_modal-services grid">
-                            <li className="services_modal-service">
-                                <i className="uil uil-check-circle
-                                 services_modal-icon"></i>
-                                <p className="services_modal-info">
-                                    I develop the user interface.</p>
-                            </li>
-
-                            <li className="services_modal-service">
-                                <i className="uil uil-check-circle
-                                 services_modal-icon"></i>
-                                <p className="services_modal-info">
-                                    Web page development.</p>
-                            </li>
-
-                            <li className="services_modal-service">
-                                <i className="uil uil-check-circle
-                                 services_modal-icon"></i>
-                                <p className="services_modal-info">
-                                    I create UX element interaction.</p>
-                            </li>
-
-                            <li className="services_modal-service">
-                                <i className="uil uil-check-circle
-                                 services_modal-icon"></i>
-                                <p className="services_modal-info">
-                                    I position your company brand.</p>
-                            </li>
-
-                            <li className="services_modal-service">
-                                <i className="uil uil-check-circle
-                                 services_modal-icon"></i>
-                                <p className="services_modal-info">
-                                   Design and mockups of products for companies.</p>
-                            </li>
-                        </ul>
-
-
-                    </div>
-                </div>
-            </div>
 
             <div className="services_content">
                 <div>
