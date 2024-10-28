1ac1313189c9fbc4da76dbc958bcc884b8a89ced
Chanukaisuru <chanukaisuru26@gmail.com>
2024-10-28 23:15:26 +0530

    modified:   portfolio/src/App.js 	new file:   portfolio/src/components/qualification/Qualification.jsx 	new file:   portfolio/src/components/qualification/qualification.css

---
+++ portfolio/src/components/qualification/Qualification.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./qualification.css";
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+        <h2 className="section_title">Qualification</h2>
+        <span className="section_subtitle">My Personel Journey</span>
+
+        <div className="qualification_container container">
+            <div className="qualification_tabs">
+                <div className="qualification_button qualification_active button--flex">
+                    <i className="uil uil-graduation-cap qualification_icon"></i>Education
+                </div>
+
+                <div className="qualification_button button--flex">
+                    <i className="uil uil-briefcase-alt qualification_icon"></i>Experience
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+};
+
+export default Qualification;
