11f0130ad1a49159a24831ae9504220137886d36
Chanukaisuru <chanukaisuru26@gmail.com>
2024-10-31 16:31:30 +0530

    new file:   portfolio/src/components/footer/Footer.jsx 	new file:   portfolio/src/components/footer/footer.css

---
+++ portfolio/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer_container container">
+            <h1 className="footer_title">Chanuka</h1>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
