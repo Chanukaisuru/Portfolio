ce424e5c208398b53cdacfe968503abc395a3d3c
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-03 12:31:37 +0530

    modified:   portfolio/src/components/work/Works.jsx

--- portfolio/src/components/work/Works.jsx
+++ portfolio/src/components/work/Works.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { projectsData } from './Data'
+import { projectsNav } from './Data'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <div>
+        <div className="work_filter">
+        {projectsNav.map ((item, index) => {
+            return <span className="work_item" key={index}>
+                    {item.name}</span>;
+            
+        })}
+    </div>
+
+        <div className="work_container container grid">
+             {projectsData.map((item) => {
+                return <WorksItems/>
+             })}
+        </div>
+    </div>
   )
 }
 
