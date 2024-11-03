2ff5c3d6317646a2b2f65300de99a7c6bbf4673b
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-03 15:47:07 +0530

    modified:   portfolio/src/components/work/WorkItems.jsx 	modified:   portfolio/src/components/work/Works.jsx 	modified:   portfolio/src/components/work/work.css

--- portfolio/src/components/work/Works.jsx
+++ portfolio/src/components/work/Works.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import{useEffect} from 'react';
+import {useState} from 'react';
 import { projectsData } from './Data';
 import { projectsNav } from './Data';
 import WorkItems from './WorkItems';
 
 const Works = () => {
+    const [item, setItem] = useState({name:'all'});
+    const [projects,setProjects] = useState([]);
+    const [active, setActive] = useState(0);
+
+    useEffect(() => {
+        if(item.name=== "all"){
+            setProjects(projectsData);
+        }
+    })
+    
   return (
     <div>
-        <div className="work_filter">
+        <div className="work_filters">
         {projectsNav.map ((item, index) => {
             return <span className="work_item" key={index}>
                     {item.name}</span>;
@@ -20,7 +32,7 @@ const Works = () => {
              })}
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
