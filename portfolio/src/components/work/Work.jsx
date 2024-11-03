82b00db1429db007529bbe836127a161b15e9abd
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-03 20:39:14 +0530

    link portfolio

--- portfolio/src/components/work/Work.jsx
+++ portfolio/src/components/work/Work.jsx
@@ -4,7 +4,7 @@ import Works from './Works';
 
 const Work = () => {
   return (
-    <section className="work section" id=" portfolio">
+    <section className="work section" id="portfolio">
         <h2 className="section_title">Portfolio</h2>
         <span className="section_subtitle">Most recent work</span>
 
