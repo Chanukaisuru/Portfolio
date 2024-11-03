cc8e1040b2611121aaece6b9bb0bd1ee971c1932
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-03 16:42:29 +0530

    modified:   portfolio/src/components/work/Works.jsx 	modified:   portfolio/src/components/work/work.css

--- portfolio/src/components/work/Works.jsx
+++ portfolio/src/components/work/Works.jsx
@@ -14,20 +14,39 @@ const Works = () => {
         if(item.name=== "all"){
             setProjects(projectsData);
         }
-    })
-    
+
+        else{
+            const newProjects = projectsData.filter((project) => {
+                return project.category === item.name;
+            });
+            setProjects(newProjects);
+
+            }
+        
+    }, [item]);
+
+
+  const handleClick = (e, index) =>{
+    setItem({name: e.target.textContent});
+    setActive(index);
+  };
   return (
     <div>
         <div className="work_filters">
         {projectsNav.map ((item, index) => {
-            return <span className="work_item" key={index}>
-                    {item.name}</span>;
+            return (<span 
+                       onClick={(e) => {handleClick(e,index);
+                       }}
+                    className={`${active === index ? 'active-work' : ''}
+                    work_item`}
+                    key={index}>
+                    {item.name}</span>);
             
         })}
     </div>
 
         <div className="work_container container grid">
-             {projectsData.map((item) => {
+             {projects.map((item) => {
                 return <WorkItems item={item} key={item.id} />;
              })}
         </div>
