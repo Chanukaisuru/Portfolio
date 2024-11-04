614bb190f6d6926e72815e6c3ecb80cd06ae74a1
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-04 22:28:15 +0530

    modified:   portfolio/src/components/about/Info.jsx 	modified:   portfolio/src/components/home/Data.jsx

--- portfolio/src/components/about/Info.jsx
+++ portfolio/src/components/about/Info.jsx
@@ -7,7 +7,7 @@ const Info = () => {
         <i class='bx bx-award'></i>
 
         <h3 className="about_title">Experience</h3>
-        <span className="about_subtitle">Year Working</span>
+        <span className="about_subtitle">Not yet</span>
       </div>
 
       <div className="about_box">
