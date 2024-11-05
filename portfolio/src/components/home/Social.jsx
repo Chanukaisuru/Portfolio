55d05c1c3de25794a513a7e24133a1b0103a756f
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-05 15:45:36 +0530

    modified:   portfolio/public/index.html 	modified:   portfolio/src/components/home/ScrollDown.jsx 	modified:   portfolio/src/components/home/Social.jsx 	modified:   portfolio/src/components/home/home.css

--- portfolio/src/components/home/Social.jsx
+++ portfolio/src/components/home/Social.jsx
@@ -1,36 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 
 const Social = () => {
+  const socialRef = useRef(null);
+
   useEffect(() => {
-    gsap.to(".home_social-icon", {
-      x: 30, // Adjust the value to change the animation distance
-      duration: 2, // Adjust the value to change the duration of the animation
-      stagger: 0.2, // Adds a slight delay between each icon animation
-    });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            // Trigger the GSAP animation when the component is in view
+            gsap.fromTo(
+              ".home_social-icon",
+              { x: -100, opacity: 0 },
+              { x: 30, opacity: 1, duration: 2, stagger: 0 }
+            );
+          }
+        });
+      },
+      { threshold: 0.1 } // Adjust this value for when you want the animation to trigger
+    );
+
+    if (socialRef.current) {
+      observer.observe(socialRef.current);
+    }
+
+    return () => {
+      if (socialRef.current) {
+        observer.unobserve(socialRef.current);
+      }
+    };
   }, []);
 
   return (
-    <div className='home_social'>
-        <a href="https://www.linkedin.com/in/chanuka-isuru-5030492b2/"
-        className='home_social-icon' target='_blank'>
-            <i className="uil uil-linkedin-alt"></i>
-        </a>
+    <div className='home_social' ref={socialRef}>
+      <a href="https://www.linkedin.com/in/chanuka-isuru-5030492b2/"
+         className='home_social-icon' target='_blank'>
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
 
-        <a href="https://medium.com/@chanukaisuru"
-        className='home_social-icon' target='_blank'>
-            <i className="uil uil-medium-m"></i>
-        </a>
+      <a href="https://medium.com/@chanukaisuru"
+         className='home_social-icon' target='_blank'>
+        <i className="uil uil-medium-m"></i>
+      </a>
 
-        <a href="https://m.facebook.com/"
-        className='home_social-icon' target='_blank'>
-            <i className="uil uil-facebook-f"></i>
-        </a>
+      <a href="https://m.facebook.com/"
+         className='home_social-icon' target='_blank'>
+        <i className="uil uil-facebook-f"></i>
+      </a>
 
-        <a href="https://github.com/Chanukaisuru"
-        className='home_social-icon' target='_blank'>
-            <i className="uil uil-github-alt"></i>
-        </a>
+      <a href="https://github.com/Chanukaisuru"
+         className='home_social-icon' target='_blank'>
+        <i className="uil uil-github-alt"></i>
+      </a>
     </div>
   );
 };
